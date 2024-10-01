2b64727857858b29def23ca64bef72bff1075e5a
Imre Boersma <imre.boersma@gmail.com>
2024-10-01 11:12:53 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,30 +1,30 @@
-function main() {
-  if (new Date().getMonth() !== 11) return;
+function main(): void {
+  const currentMonth: number = new Date().getMonth();
+  if (currentMonth !== 11) return;
 
-  const original: GoogleAppsScript.Spreadsheet.Sheet | null = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Huidig jaar');
-  const backup: GoogleAppsScript.Spreadsheet.Sheet = SpreadsheetApp.getActiveSpreadsheet().insertSheet(`Backup ${new Date().getFullYear()}`);
-
+  const spreadsheet: GoogleAppsScript.Spreadsheet.Spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  const original: GoogleAppsScript.Spreadsheet.Sheet | null = spreadsheet.getSheetByName('Huidig jaar');
+  
   if (!original) {
     throw new Error('Sheet not found');
   }
 
-  const source: GoogleAppsScript.Spreadsheet.Range = original.getRange(1, 1, original.getLastRow(), original.getLastColumn());
-  const destination: GoogleAppsScript.Spreadsheet.Range = backup.getRange(1, 1, source.getNumRows(), source.getNumColumns());
+  const backup: GoogleAppsScript.Spreadsheet.Sheet = spreadsheet.insertSheet(`Backup ${new Date().getFullYear()}`);
+  const sourceRange: GoogleAppsScript.Spreadsheet.Range = original.getDataRange();
+  const destinationRange: GoogleAppsScript.Spreadsheet.Range = backup.getRange(1, 1, sourceRange.getNumRows(), sourceRange.getNumColumns());
 
-  source.copyTo(destination);
-
-  original.deleteRows(2, original.getLastRow());
+  sourceRange.copyTo(destinationRange);
+  original.deleteRows(2, original.getLastRow() - 1);
 
   formatBackup(backup);
 }
 
-function formatBackup(backup: GoogleAppsScript.Spreadsheet.Sheet) {
+function formatBackup(backup: GoogleAppsScript.Spreadsheet.Sheet): void {
   backup.deleteColumn(1);
   backup.autoResizeColumns(1, 5);
   backup.setFrozenRows(1);
   backup.setTabColor('#434343');
 
   const backupProtection: GoogleAppsScript.Spreadsheet.Protection = backup.protect();
-
   backupProtection.setDescription(`Dit is een backup van ${new Date().getFullYear()}. Deze sheet is alleen-lezen.`);
 }
